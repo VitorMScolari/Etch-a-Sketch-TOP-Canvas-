c7a49d5718974bc5aa9d62d4af626f9642d019d5
VitorMScolari <vmarcals@hotmail.com>
2021-08-07 17:44:44 -0300

    first statements and functions

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-const divs = document.createElement('div');
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 const button = document.querySelector('.button');
+const ctx = container.getContext("2d");
+
+let painting = false;
+
 
 for(let i=0; i < 10; i++) {
     const divs = document.createElement('div');
     divs.classList.add('main');
-    container.append(divs);
+    ctx.appendChild(divs);
 }
+
+const main = document.querySelectorAll('.main');
+
+
+function mainDraw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round'
+
+
+    ctx.strokeStyle = 'green';
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY)
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+}
+
+function startDrawing() {
+    painting = true;
+}
+
+function stopDrawing() {
+     painting = false;
+     ctx.beginPath();
+}
+
+
+main.forEach(main => main.addEventListener('mousemove', mainDraw))
+main.forEach(main => main.addEventListener('mousedown', startDrawing))
+main.forEach(main => main.addEventListener('mouseup', stopDrawing))
 
 
 /*
-function gridNumbers(e) {
-    div.remove(divs);
+function buttonPop {
+    
 }
-
 */
-
 
 /*
 button.addEventListener('click', gridNumbers);
-
 */
